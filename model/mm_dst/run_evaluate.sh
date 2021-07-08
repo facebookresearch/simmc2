75f5de0bcda22ccfc7b9d934bf14823456fe8c4b
Seungwhan Moon <me@shanemoon.com>
2021-07-08 13:45:20 -0700

    Update run_evaluate.sh

--- model/mm_dst/run_evaluate.sh
+++ model/mm_dst/run_evaluate.sh
@@ -10,11 +10,7 @@ fi
 
 # Evaluate (Example)
 python -m utils.evaluate_dst \
-    --input_path_target="${PATH_DATA_DIR}"/simmc_fashion/fashion_devtest_dials.json \
-    --input_path_predicted="${PATH_DIR}"/fashion_devtest_pred_dials.json \
-    --output_path_report="${PATH_DIR}"/fashion_report.json
+    --input_path_target="${PATH_DATA_DIR}"/simmc2_dials_dstc10_devtest_dials.json \
+    --input_path_predicted="${PATH_DIR}"/simmc2_dials_dstc10_devtest_pred_dials.json \
+    --output_path_report="${PATH_DIR}"/simmc2_dials_dstc10_report.json
 
-python -m utils.evaluate_dst \
-    --input_path_target="${PATH_DATA_DIR}"/simmc_furniture/furniture_devtest_dials.json \
-    --input_path_predicted="${PATH_DIR}"/furniture_devtest_pred_dials.json \
-    --output_path_report="${PATH_DIR}"/furniture_report.json
