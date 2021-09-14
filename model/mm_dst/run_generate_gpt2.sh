5ea2b91ce418bd86740c6e5d467e497be16ed3ec
Satwik Kottur <satwik27@gmail.com>
2021-09-14 12:03:18 -0700

    Running retrieval evaluation for GPT-2 model

--- model/mm_dst/run_generate_gpt2.sh
+++ model/mm_dst/run_generate_gpt2.sh
@@ -7,19 +7,19 @@ else
 fi
 
 # Generate sentences (Furniture, multi-modal)
-# python -m gpt2_dst.scripts.run_generation \
-#     --model_type=gpt2 \
-#     --model_name_or_path="${PATH_DIR}"/gpt2_dst/save/model/ \
-#     --num_return_sequences=1 \
-#     --length=100 \
-#     --stop_token='<EOS>' \
-#     --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/simmc2_dials_dstc10_devtest_predict.txt \
-#     --path_output="${PATH_DIR}"/gpt2_dst/results/simmc2_dials_dstc10_devtest_predicted.txt
-
-
-python -m gpt2_dst.scripts.run_retrieval \
+python -m gpt2_dst.scripts.run_generation \
     --model_type=gpt2 \
-    --length=256 \
     --model_name_or_path="${PATH_DIR}"/gpt2_dst/save/model/ \
-    --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/simmc2_dials_dstc10_devtest_retrieval.txt \
+    --num_return_sequences=1 \
+    --length=100 \
+    --stop_token='<EOS>' \
+    --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/simmc2_dials_dstc10_devtest_predict.txt \
     --path_output="${PATH_DIR}"/gpt2_dst/results/simmc2_dials_dstc10_devtest_predicted.txt
+
+
+# python -m gpt2_dst.scripts.run_retrieval \
+#     --model_type=gpt2 \
+#     --length=256 \
+#     --model_name_or_path="${PATH_DIR}"/gpt2_dst/save/model/ \
+#     --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/simmc2_dials_dstc10_devtest_retrieval.txt \
+#     --path_output="${PATH_DIR}"/gpt2_dst/results/simmc2_dials_dstc10_devtest_predicted.txt
